a197740ade5ca70b8aa7063aaea84144687e55b8
Isaiah-Wilson12 <Isaiah.Wilson@hgs.hiddengeniusproject.org>
2021-11-12 02:55:25 +0000

    Made a variable that printed out my KPI and time manant tips, ent tips, and commitments.

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+    String name = "Isaiah Wilson";
+    String timeManagement = "focusing on important after school activities before fun activites";
+    String personalCommitment = "Focusing in school and other important after school activties";
+    String KPIMetrics = "how many people are buying the product";
+
+    System.out.println("Hello my name is " + name + ", the time management strategy that I would like to be held accountable\n"
+        + "to is " + timeManagement + ". I will personally commit to " + personalCommitment + ".\n "
+              + "My business' main KPI is/are " + KPIMetrics + ".");
+     }
+}
